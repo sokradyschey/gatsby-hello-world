42ccd72273ccad7ebf2364eaa76cd79d15f25694
Sokrady Chey <sokradychey@gmail.com>
2019-03-21 20:44:03 -0400

    Learning page linking

---
+++ src/pages/about.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Header from "../components/header"
+
+export default () => (
+    <div style={{ color: `teal` }}>
+        <Header headerText="About Gatsby" arbitraryPhrase="is arbitrary" />
+        <Header headerText="It's pretty cool" />
+        <p>Such Wow! Very React!</p>
+    </div>
+)
